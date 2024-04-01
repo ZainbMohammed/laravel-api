8c01e49a3f4a9e2b1857c0002264b55c902d31af
Zinam Mohamed <pk.x3311@gmail.com>
2024-04-01 07:40:49 +0300

    Create token for logined user

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class userController extends Controller
 {
@@ -24,11 +25,18 @@ class userController extends Controller
     function login(Request $req){
         $user = User::where('email',$req->input('email'))->first();
         if(!$user){
-            return response()->json(["message" => "user not found"]);
+            return response()->json(["message" => "user not found"],401);
         }
-        if(!$user->password != $req->input('password')){
-            return response()->json(["message" => "wrong password"]);
+        // if(!$user->password != $req->input('password')){
+        //     return response()->json(["message" => "wrong password"]);
+        // }
+
+        // لان الباسورد يتم تشفيرها باي ديفولت فلما يجي يقارن يطلع نتيجه المقارنه فولصت ف الافضل نفك التشفير وبعده نقارن
+        if(Hash::check($req->input('password'),$user->password)){
+            return response()->json(["message" => "wrong password"],401);
         }
-        return $user;
+
+        $token = $user->createToken('auth_token');
+        return response()->json(["token" => $token->plainTextToken]);
     }
 }
