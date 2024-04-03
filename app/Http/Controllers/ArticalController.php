1fbd6fd64fdf269759b78ec5117c80dca4f480d2
Zinam Mohamed <pk.x3311@gmail.com>
2024-04-03 23:08:50 +0300

    updated artical controller

--- app/Http/Controllers/ArticalController.php
+++ app/Http/Controllers/ArticalController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class ArticalController extends Controller
 {
-    //
+    // add new artical
     function createArtical(Request $req){
         $artical = Articles::create([
             "title" => $req->input('title'),
@@ -16,21 +16,26 @@ class ArticalController extends Controller
         return $artical;
     }
 
+    // display all articales
     function getAllArticals(Request $req){
         $articals = Articles::all();
         return $articals;
     }
+
+    // display only one articale based on id
     function getArtical($id){
         $artical = Articles::find($id);
         return $artical;
     }
 
+    // delete articale from DB
     function deleteArtical($id){
         $artical = Articles::find($id);
         $artical->delete();
         return 'artical deleted successful';
     }
 
+    // update one articale based on id
     function updateArtical(Request $req, $id){
         $artical = Articles::find($id);
         $artical->title = $req->input('title');
